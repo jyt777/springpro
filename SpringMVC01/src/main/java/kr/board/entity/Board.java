9dfa5e5c8502dbac1f653476e60dbeafa9471a3b
정용태 <@118.222.142.105>
2023-03-07 10:20:43 +0900

    mvc01

---
+++ SpringMVC01/src/main/java/kr/board/entity/Board.java
@@ -0,0 +1,14 @@
+package kr.board.entity;
+
+//Lombok API (setter, getter 자동생성)
+public class Board {
+	private int idx;	//번호
+	private String title; // 제목
+	private String content; //내용
+	private String writer; //작성자
+	private String indate; //작성일
+	private int count;		//조회수
+	
+	//setter, getter
+
+}
